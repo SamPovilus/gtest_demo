d29d23507fc3e32fc5c657515f03dcd8a820745b
Sam Povilus <kernel.development@povil.us>
2019-09-20 20:25:18 -0600

    almost working

--- unit_test/rectangle_test.cpp
+++ unit_test/rectangle_test.cpp
@@ -1,6 +1,11 @@
 #include <gtest/gtest.h>
 #include "rectangle.hpp"
 
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
 TEST (RectangleTest, area) { 
     auto testRect = new Rectangle();
     testRect->set_height(1);
