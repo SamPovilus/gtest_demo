e2fbef5d3e93a14a254b7de37ab9c1b2bbccac59
Sam Povilus <kernel.development@povil.us>
2019-09-20 19:43:14 -0600

    not working but started gtest

---
+++ unit_test/rectangle_test.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "rectangle.hpp"
+
+TEST (RectangleTest, area) { 
+    auto testRect = new Rectangle();
+    testRect->set_height(1);
+    testRect->set_width(1);
+    EXPECT_EQ (1, testRect->get_area());
+    testRect->set_width(2);
+    EXPECT_EQ (2, testRect->get_area());
+
+    testRect->set_height(2);
+    EXPECT_EQ (4, testRect->get_area());]
+}
+
+TEST (Rectangle, zero) { 
+    auto testRect = new Rectangle();
+    testRect->set_height(1);
+    testRect->set_width(1);
+    EXPECT_EQ (1, testRect->get_area());
+    testRect->set_width(0);
+    EXPECT_EQ (0, testRect->get_area());
+}
